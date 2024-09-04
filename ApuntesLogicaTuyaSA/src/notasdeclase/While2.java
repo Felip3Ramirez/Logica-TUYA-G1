27e87c4615e2d459f57c0502c0431803e9561d25
BraiderStid <>
2024-09-04 15:32:11 -0500

    Update While2.java

--- ApuntesLogicaTuyaSA/src/notasdeclase/While2.java
+++ ApuntesLogicaTuyaSA/src/notasdeclase/While2.java
@@ -5,13 +5,14 @@ public class While2 {
         int i = 0;
 
         while (i < 100){
-            System.out.println(+i);
-            i++;
+
+
             if (i % 2 == 0){
-                System.out.print("el numero es par ");
+                System.out.println(i+" el numero es par ");
             }else {
-                System.out.print("el numero es inpar ");
+                System.out.println(i+" el numero es inpar ");
             }
+            i++;
         }
 
     }
