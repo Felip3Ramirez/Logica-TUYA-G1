c04b04844a6d7fe320ebf80b4f8f1ded7a7d6d5c
BraiderStid <>
2024-09-04 16:34:58 -0500

    update

---
+++ ApuntesLogicaTuyaSA/src/notasdeclase/Arrays.java
@@ -0,0 +1,10 @@
+package notasdeclase;
+
+public class Arrays {
+    public static void main(String[] args) {
+
+        int [] edades ={16,41,20,31,24};
+
+
+    }
+}
---
+++ ApuntesLogicaTuyaSA/src/notasdeclase/WhileAcumulador.java
@@ -0,0 +1,15 @@
+package notasdeclase;
+
+public class WhileAcumulador {
+    public static void main(String[] args) {
+
+        int i = 0;
+        int suma = 0;
+
+        while (i<=10){
+            suma=suma+i;
+            System.out.println(suma);
+            i++;
+        }
+    }
+}
